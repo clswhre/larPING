c902677b8e15e13c2128ef92050c0d02d7c508a0
Bohdan Kryzhov <kb251_kbs@student.ztu.edu.ua>
2026-08-30 08:37:36 +0300

    [+] 4DD3D L4RP 73XT B351D35 R3C31V3D 1CMP 1NF0

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand/v2"
 	"os"
 	"os/signal"
 	"time"
@@ -30,6 +31,30 @@ const (
 
 	colorReset = "\033[0m"
 )
+
+var phrases = []string{
+	"[*] 8YP4551NG M41NFR4M3 F1R3W4LL...",
+	"[!] 1NJ3CT1NG P4YL04D...",
+	"[*] D3CRYPT1NG P455W0RD H45H35...",
+	"[+] D0WN104D1NG S3CR3T D4T4...",
+	"[!] TR4C3 D3T3CT3D, R0UT1NG...",
+	"[*] 1N1T14L1Z1NG 3NCRYPT10N BYP455...",
+	"[!] 3XPL01T1NG 0V3RFL0W VULN...",
+	"[*] 3NUM3R4T1NG H1DD3N D1R3CT0R13S...",
+	"[+] 4CC3SS GR4NT3D. W3LC0M3 T0 TH3 M41NFR4M3...",
+	"[!] 5Y5T3M 4L3RT: 1NTRUD3R D3T3CT3D. 3V4D1NG...",
+	"[*] 1NJ3CT1NG R3V3R5E 5H3LL C0D3...",
+	"[+] P4YLOAD D3L1V3R3D. 3X3CUT1NG...",
+	"[!] F1R3W4LL RUL3S BYP4553D...",
+	"[*] CR4CK1NG W1F1 K3Y 4LG0R1THM...",
+	"[+] D4T4B4S3 DUMP 1N PR0GR3SS...",
+	"[!] R00TK1T 1NST4LL3D. P3R515T3NC3 3N4BL3D...",
+	"[*] 5C4NN1NG P0RT5 1337-65535...",
+	"[+] 5H3LL 4CC3SS 0BT41N3D. 3SC4L4T1NG PR1V1L3G3S...",
+	"[!] 1D5 3V4D3D. CH4NG1NG M4C 4DDR3SS...",
+	"[*] 3R4S1NG L0G5. N0 TR4C3 L3FT...",
+	"[+] M1SS10N C0MPL3T3. 3X1T...",
+}
 
 func main() {
 	check_args()
@@ -109,9 +134,10 @@ func ping_target(ip string) {
 	pinger.OnRecv = func(pkt *probing.Packet) {
 
 		// add some fun things here
-
-		fmt.Printf("%s[DATA_RECV] %d bytes from %s: icmp_seq=%d time=%v%s\n",
-			colorWhite, pkt.Nbytes, pkt.IPAddr, pkt.Seq, pkt.Rtt.Round(time.Millisecond), colorReset)
+		print_larp_text()
+		pcktMsg := fmt.Sprintf("%d 8yt35 fr0m %s | 1cmp_53q=%d | 71m3=%v",
+			pkt.Nbytes, pkt.IPAddr, pkt.Seq, pkt.Rtt.Round(time.Millisecond))
+		print_text_ln(pcktMsg, colorBlue)
 	}
 
 	// listen for ctrl+c
@@ -136,7 +162,13 @@ func ping_target(ip string) {
 	headerMsg := fmt.Sprintf("--- %s t4rg3t st4ts ---", stats.Addr)
 	print_text_ln(headerMsg, colorCyan)
 
-	statsMsg := fmt.Sprintf("%d packets transmitted, %d received, %v%% packet loss",
+	statsMsg := fmt.Sprintf("%d packets transmitted, %d received, %3.2f%% packet loss",
 		stats.PacketsSent, stats.PacketsRecv, stats.PacketLoss)
 	print_text_ln(statsMsg, colorWhite)
+}
+
+func print_larp_text() {
+
+	randomInt := rand.IntN(len(phrases))
+	fmt.Println("\n", phrases[randomInt])
 }
